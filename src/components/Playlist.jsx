c05857804fe4878f9f01e75e90691b0927ac16df
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-08-05 19:29:04 +0300

    updated Playlist.jsx

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 
-const Playlist = () => {
+const Playlist = ({ tracks }) => {
   const [playlistName, setPlaylistName] = useState('My Playlist');
-  const [playlistTracks, setPlaylistTracks] = useState([]);
 
   const renamePlaylist = (e) => {
     setPlaylistName(e.target.value);
-  };
-
-  const removeTrackFromPlaylist = (trackId) => {
-    setPlaylistTracks(playlistTracks.filter(track => track.id !== trackId));
   };
 
   return (
@@ -21,10 +16,9 @@ const Playlist = () => {
         placeholder="Rename Playlist"
       />
       <ul>
-        {playlistTracks.map((track) => (
+        {tracks.map((track) => (
           <li key={track.id}>
             {track.name} by {track.artists[0].name}
-            <button onClick={() => removeTrackFromPlaylist(track.id)}>Remove</button>
           </li>
         ))}
       </ul>
